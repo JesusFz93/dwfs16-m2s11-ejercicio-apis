c578e8349d7d10f007ac4cab526ee7401e33eef6
Jesus <jesus.fdz93@gmail.com>
2023-04-15 10:55:28 -0500

    repaso

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
-console.log("Hola");
+const promesa1 = new Promise((resolve, reject) => {
+  const usuarios = [];
+
+  if (usuarios.length > 0) {
+    const resp = {
+      usuarios,
+      msg: "Usuarios obtenidos",
+    };
+
+    resolve(resp);
+  } else {
+    const resp = {
+      usuarios,
+      msg: "No existen usuarios",
+    };
+
+    reject(resp);
+  }
+});
+
+const llamarApisPromise = () => {
+  promesa1
+    .then((respuesta) => {
+      console.log(respuesta);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const llamarApisAsyncAwait = async () => {
+  try {
+    const respuesta = await promesa1;
+    console.log(respuesta);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// llamarApisPromise();
+llamarApisAsyncAwait();
