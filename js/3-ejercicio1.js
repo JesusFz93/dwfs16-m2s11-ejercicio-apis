a3463d8c663216ddfbffc1b27d87d287ca334ee8
Jesus <jesus.fdz93@gmail.com>
2023-04-15 13:00:15 -0500

    ejercicio1

---
+++ js/3-ejercicio1.js
@@ -0,0 +1,13 @@
+const inputNombreCiudad = document.getElementById("inputNombreCiudad");
+
+const consultarClima = async () => {
+  try {
+    const respuesta = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${inputNombreCiudad.value}&appid=616629f9acdc3b22b8b09553e632e5da`
+    );
+    const datos = await respuesta.json();
+    console.log(datos);
+  } catch (error) {
+    console.log(error);
+  }
+};
